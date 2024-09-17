95141e849c7dd15461e616129a2f2b4c471e4aa3
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-09-17 19:08:11 +0900

    feat: make header block prettier

--- src/App.tsx
+++ src/App.tsx
@@ -113,26 +113,33 @@ function App() {
 
   return (
     <div className='flex items-center justify-center h-screen'>
-      <div className='grid grid-flow-row gap-2 max-w-lg'>
+      <div className='grid grid-flow-row gap-5 max-w-lg'>
         {/* Header */}
-        <div className='grid grid-cols-3'>
-          <div className='font-sans font-extrabold m-2 text-5xl'>
+        <div className='flex flex-col sm:flex-row justify-between items-center w-full space-y-2'>
+          {/* Game Title */}
+          <div className='font-sans font-extrabold text-7xl'>
             2048
           </div>
-          <div className='box-border border-2 border-black m-2'>
-            <div className='font-bold'>
-              Score
+
+          {/* Score and Best Score Container */}
+          <div className='flex space-x-4'>
+            {/* Score Block */}
+            <div className='flex flex-col items-center bg-gray-200 rounded-lg shadow-md p-4'>
+              <div className='text-sm font-semibold text-gray-700'>
+                SCORE
+              </div>
+              <div className='text-2xl font-bold text-gray-900'>
+                {score}
+              </div>
             </div>
-            <div>
-              {score}
-            </div>
-          </div>
-          <div className='box-border border-2 border-black m-2'>
-            <div className='font-bold'>
-              Best
-            </div>
-            <div>
-              {bestScore}
+            {/* Best Score Block */}
+            <div className='flex flex-col items-center bg-gray-200 rounded-lg shadow-md p-4'>
+              <div className='text-sm font-semibold text-gray-700'>
+                BEST
+              </div>
+              <div className='text-2xl font-bold text-gray-900'>
+                {bestScore}
+              </div>
             </div>
           </div>
         </div>
