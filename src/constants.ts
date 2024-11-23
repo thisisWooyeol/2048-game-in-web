3e1a7c7a68b58d9928eb37c8ca08be68c415df98
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-11-23 17:11:58 +0900

    🎨 Refactor constants and move ARROW_KEYS to improve code organization and reusability

--- src/constants.ts
+++ src/constants.ts
@@ -1,31 +1,42 @@
-export type Cell = number | null;
-export type Map2048 = Cell[][];
+type Cell = number | null;
+type Map2048 = Cell[][];
 
-export enum Direction {
+enum Direction {
   Up,
   Right,
   Down,
   Left,
 }
 
-export type RotateDegree = 0 | 90 | 180 | 270;
+const ARROW_KEYS = [
+  'ArrowUp',
+  'ArrowDown',
+  'ArrowLeft',
+  'ArrowRight',
+  'w',
+  'a',
+  's',
+  'd',
+];
+
+type RotateDegree = 0 | 90 | 180 | 270;
 type DirectionDegreeMap = Record<Direction, RotateDegree>;
 
-export const rotateDegreeMap: DirectionDegreeMap = {
+const rotateDegreeMap: DirectionDegreeMap = {
   [Direction.Up]: 90,
   [Direction.Right]: 180,
   [Direction.Down]: 270,
   [Direction.Left]: 0,
 };
 
-export const revertDegreeMap: DirectionDegreeMap = {
+const revertDegreeMap: DirectionDegreeMap = {
   [Direction.Up]: 270,
   [Direction.Right]: 180,
   [Direction.Down]: 90,
   [Direction.Left]: 0,
 };
 
-export const stringDirectionMap: Record<string, Direction> = {
+const stringDirectionMap: Record<string, Direction> = {
   ArrowUp: Direction.Up,
   ArrowRight: Direction.Right,
   ArrowDown: Direction.Down,
@@ -36,15 +47,32 @@ export const stringDirectionMap: Record<string, Direction> = {
   a: Direction.Left,
 };
 
-export type MoveResult = {
+type MoveResult = {
   result: Map2048;
   isMoved: boolean;
   newPoints: number;
 };
 
-export type State2048 = {
+type State2048 = {
   map: Map2048;
   score: number;
   bestScore: number;
   gameStatus: 'playing' | 'win' | 'lose';
+};
+
+export type {
+  Cell,
+  Map2048,
+  RotateDegree,
+  DirectionDegreeMap,
+  MoveResult,
+  State2048,
+};
+
+export {
+  ARROW_KEYS,
+  Direction,
+  rotateDegreeMap,
+  revertDegreeMap,
+  stringDirectionMap,
 };
